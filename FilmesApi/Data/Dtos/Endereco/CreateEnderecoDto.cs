008b1041909fcacb2a601ad7d41c0d4e252b6a06
raphael-aciardi <996193248ph@gmail.com>
2024-08-27 14:25:46 -0300

    Ajustando o mapper de cinema e inserindo relações no get de cinema com o endereço

--- FilmesApi/Data/Dtos/Endereco/CreateEnderecoDto.cs
+++ FilmesApi/Data/Dtos/Endereco/CreateEnderecoDto.cs
@@ -2,5 +2,7 @@
 {
     public class CreateEnderecoDto
     {
+        public string Logradouro { get; set; }
+        public int Numero { get; set; }
     }
 }
--- FilmesApi/Data/Dtos/Endereco/ReadEnderecoDto.cs
+++ FilmesApi/Data/Dtos/Endereco/ReadEnderecoDto.cs
@@ -1,6 +1,13 @@
-﻿namespace FilmesApi.Data.Dtos.Endereco
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Data.Dtos.Endereco
 {
     public class ReadEnderecoDto
     {
+        public int Id { get; set; }
+        [Required]
+        public string Logradouro { get; set; }
+        [Required]
+        public int Numero { get; set; }
     }
 }
